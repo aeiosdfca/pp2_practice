3e438a3c9d4acb02fa840ccc43080443ff8148fd
Igor Tsoy <129654955+lycorince@users.noreply.github.com>
2026-05-02 17:33:15 +0500

    Update functions.sql

--- TSIS/TSIS1/functions.sql
+++ TSIS/TSIS1/functions.sql
@@ -2,16 +2,20 @@ CREATE OR REPLACE FUNCTION search_contacts(p_query TEXT)
 RETURNS TABLE(name TEXT, email TEXT, phone TEXT, group_name TEXT) AS $$
 BEGIN
     RETURN QUERY
-    SELECT p.name, p.email, ph.phone, g.name
+    SELECT 
+        p.name::text,
+        p.email::text,
+        ph.phone::text,
+        g.name::text
     FROM phonebook p
     LEFT JOIN phones ph ON p.id = ph.contact_id
     LEFT JOIN groups g ON p.group_id = g.id
-    WHERE p.name ILIKE '%'||p_query||'%'
-       OR p.email ILIKE '%'||p_query||'%'
-       OR ph.phone ILIKE '%'||p_query||'%';
+    WHERE 
+        p.name ILIKE '%' || p_query || '%'
+        OR p.email ILIKE '%' || p_query || '%'
+        OR ph.phone ILIKE '%' || p_query || '%';
 END;
 $$ LANGUAGE plpgsql;
-
 
 CREATE OR REPLACE FUNCTION get_contacts_page(lim INT, off INT)
 RETURNS TABLE(name VARCHAR, email VARCHAR) AS $$
